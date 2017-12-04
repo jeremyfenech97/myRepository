381ba362ebb58ba4de2f4139f1bed85a2199d47a
jeremyfenech97 <33456121+jeremyfenech97@users.noreply.github.com>
2017-12-04 10:18:15 +0100

    ex3

---
+++ ex3/Assets/move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public float speed = 5f;
+
+	void Update () {
+		Vector3 pos = transform.position;
+
+		if (Input.GetKey ("w")) {
+			pos.y += speed * Time.deltaTime;
+		}
+		if (Input.GetKey ("s")) {
+			pos.y -= speed * Time.deltaTime;
+		}
+		if (Input.GetKey ("d")) {
+			pos.x += speed * Time.deltaTime;
+		}
+		if (Input.GetKey ("a")) {
+			pos.x -= speed * Time.deltaTime;
+		}
+
+
+		transform.position = pos;
+	
+}
+}
