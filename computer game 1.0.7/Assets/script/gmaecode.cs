2f02c9a8033b6aafd92e3c47ab94a67e941f1a24
jeremyfenech97 <33456121+jeremyfenech97@users.noreply.github.com>
2017-12-15 15:46:26 +0100

    Game vr 8
    -moving from start to level 1
    -level 3 obsicals moving
    -game bild

---
+++ computer game 1.0.7/Assets/script/gmaecode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gmaecode : MonoBehaviour {
+	
+
+
+	void Start () {
+		
+	}
+	public void LoadLevel(string name)
+	{
+		print("Level Load requested for " + name);
+		SceneManager.LoadScene (name);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
